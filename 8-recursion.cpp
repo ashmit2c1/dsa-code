0ca07b2a7fcf4702cabf9208ac93b412f88c1104
Ashmit <codeatashmit@gmail.com>
2024-06-07 15:11:45 +0530

    8-recursion

---
+++ 8-recursion.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// ======================= // 
+// PRINT RECURSIVELY // 
+// ======================= // 
+void printRecursion(int i, string s, int n){
+    if(i>n){
+        return;
+    }
+    cout << s << "\n";
+    printRecursion(i+1,s,n);
+}
+// ======================= // 
+// PRINT RECURSIVELY FROM 1 TO N // 
+// ======================= // 
+void print1ToN(int i,int n){
+    if(i>n){
+        return;
+    }
+    cout << i << "\n";
+    print1ToN(i+1,n);
+}
+// ======================= // 
+// PRINT RECURSIVELY FROM N to 1 // 
+// ======================= // 
+void printNTo1(int i,int n){
+    if(i<1){
+        return;
+    }
+    cout << i << "\n";
+    printNTo1(i-1,n);
+}
+int main(){
+    int n = 5 ;
+    string s = "simran";
+    // printRecursion(1,s,n);
+    printNTo1(n,n);
+    
+}
