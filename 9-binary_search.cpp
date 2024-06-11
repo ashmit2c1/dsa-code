291b6dc347ce07cf82c12767534f5d74c97d5ed3
Ashmit <codeatashmit@gmail.com>
2024-06-11 17:06:24 +0530

    9-binary-search

--- 9-binary_search.cpp
+++ 9-binary_search.cpp
@@ -169,3 +169,59 @@ bool isPresent(vector<vector<int>> &matrix, int X){
     }
     return false;
 }
+// ======================================= // 
+// FIND THE PEAK ELEMENT IN MOUNTAIN ARRAY // 
+// ======================================= //
+int findPeakElement(vector<int> arr){
+    int n = arr.size();
+    int start =0 ;
+    int end = n-1;
+    while(start<=end){
+        int mid = end+(start-end)/2;
+        if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+            return mid; // index found
+        }
+        if(arr[mid]>arr[mid-1]){
+            start=mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return -1; // index not found
+} 
+// ======================================= // 
+// SEARCH FOR ELEMENT IN ROTATED ARRAY // 
+// ======================================= //
+bool search(vector<int> arr, int x){
+    int n = arr.size();
+    int start =0 ;
+    int end = n-1;
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if(arr[mid]==x){
+            return true;
+        }
+        else if(arr[mid]==arr[start] && arr[end]==arr[mid]){
+            start ++ ;
+            end --;
+        }
+        else if(arr[mid]>=arr[start]){
+            if(x>=arr[start] && x<arr[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        else{
+            if(x>=arr[mid] && x<arr[end]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+    }
+    return false;
+}
