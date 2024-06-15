2ef1a2074ad1fae23ae4e2bfffaca8147c108279
Ashmit <codeatashmit@gmail.com>
2024-06-15 09:59:00 +0530

    10-binary-tree

--- 10-binary-tree.cpp
+++ 10-binary-tree.cpp
@@ -74,6 +74,95 @@ TreeNode<int>* takeInputLevelWise(){
     }
     return root;
 }
+// LEVEL ORDER TRAVERSL IN BINARY TREE
+vector<vector<int>> levelOrderTraveralBinaryTree(TreeNode<int>* root){
+    vector<vector<int>> ans;
+    if(root==NULL){
+        return ans;
+    }
+    queue<TreeNode<int>*> q;
+    q.push(root);
+    while(q.empty()==false){
+        vector<int> level;
+        int n = q.size();
+        for(int i = 0; i<n ; i++){
+            TreeNode<int>* frontNode = q.front();
+            if(frontNode->left!=NULL){
+                q.push(frontNode->left);
+
+            }
+            if(frontNode->right!=NULL)
+            {
+                q.push(frontNode->right);
+            }
+            level.push_back(frontNode->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+// COUNT NUMBER OF NODES PRESENT IN BINARY TREE
+int countNodesBinaryTree(TreeNode<int>* root){
+    if(root==NULL){
+        return 0;
+    }
+    return countNodesBinaryTree(root->left) + countNodesBinaryTree(root->right)+1;
+}
+// INORDER TRAVERSAL OF BINARY TREE
+void inOrder(TreeNode<int>* root, vector<int> &v){
+    if(root==NULL){
+        return;
+    }
+    inOrder(root->left,v);
+    v.push_back(root->data);
+    inOrder(root->right,v);
+}
+
+vector<int> inOrderTraversalBinaryTree(TreeNode<int>* root){
+    vector<int> ans;
+    inOrder(root,ans);
+    return ans;
+}
+// MAX DEPTH OR HEIGHT OF BINARY TREE
+int maxDepth(TreeNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+    int height = max(maxDepth(root->left),maxDepth(root->right));
+    return height+1;
+}
+// CHECK WHETHER A PARTICULAR VALUE IS PRESENT IN BINARY TREE OR NOT
+bool searchForKey(TreeNode<int>* root, int key){
+    if(root==NULL){
+        return false;
+    }
+    if(root->data==key){
+        return true;
+    }
+    return (searchForKey(root->left,key) || searchForKey(root->right,key));
+}
+// FIND THE MINIUM VALUE IN BINARY TREE
+int findMinValueBinaryTree(TreeNode<int>* root){
+    if(root==NULL){
+        return INT_MAX;
+    }
+    int ans = root->data;
+    int leftMin = findMinValueBinaryTree(root->left);
+    int rightMin = findMinValueBinaryTree(root->right);
+    ans = min(ans,min(rightMin,leftMin));
+    return ans;
+}
+// FIND THE MAX VALUE IN BINARY TREE
+int findMaxValueBinaryTree(TreeNode<int>* root){
+    if(root==NULL){
+        return INT_MIN;
+    }
+    int ans = root->data;
+    int leftMax = findMaxValueBinaryTree(root->left);
+    int rightMax = findMaxValueBinaryTree(root->right);
+    ans = max(ans,max(leftMax,rightMax));
+    return ans;
+}
 int main(){
     // CREATING THE TREE NODES DYNAMICALLY
     TreeNode<int>* root = new TreeNode<int>(1);
@@ -83,23 +172,4 @@ int main(){
     root->left=n1;
     root->right=n2;
     
-}
-
-int findMax(vector<int> arr, int k){
-    int n = arr.size();
-    int ptr=0;
-    int ans=-1;
-    int zero =0;
-    for(int i = 0; i<n ; i++){
-        if(arr[i]==0){
-            zero++;
-        }
-        while(zero>k){
-            if(arr[ptr]==0){
-                zero--;
-            }
-            ptr++;
-        }
-        ans = max(ans,i-ptr+1);
-    }
 }
