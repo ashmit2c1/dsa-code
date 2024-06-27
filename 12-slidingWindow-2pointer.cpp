70495822cb842fb000ef1f69cc9a93b236bbc3c8
Ashmit <codeatashmit@gmail.com>
2024-06-27 15:03:16 +0530

    12 - sliding-window & 2 pointer

--- 12-slidingWindow-2pointer.cpp
+++ 12-slidingWindow-2pointer.cpp
@@ -57,3 +57,45 @@ int maxSum(vector<int> &v, int k){
     }
     return maxSum;
 }
+// FIND THE MAX POINTS FROM THE CARDS WE CAN PICK 
+/* We can pick N cards from the array to maximise points, however 
+we can only pick the cards from either the front or the back, that is 
+if we have to pick 4 cards, then we either pick all 4 from the front 
+3 from front 1 from back ,2 from front 2 fron back , 1 front 3 back or 
+all four from the back, we cannot pick cards from the middle */
+int maxPoints(vector<int> &v, int k){
+    int n = v.size();
+    int leftSum = 0 ;
+    int rightSum = 0;
+    int maxSum = 0 ;
+    for(int i = 0; i<k ; i++){
+        leftSum+=v[i];
+    }
+    maxSum = leftSum;
+    int rightIndex = n-1;
+    for(int i=k-1;i>=0;i--){
+        leftSum-=v[i];
+        rightSum+=v[rightIndex];
+        maxSum = max(maxSum,leftSum+rightSum);
+        rightIndex--;
+    }
+    return maxSum;
+}
+// LONGEST SUBSTRING WITHOUT REPEARTING CHARACTERS
+int longestSubString(string s){
+    int first=0;
+    int second =0;
+    int len = 0;
+    vector<bool> count(256,false);
+    while(second<s.size()){
+        char character = s[second];
+        while(count[character]){
+            count[s[first]]=false;
+            first++;
+        }
+        count[character] = true;
+        len = max(len, second-first+1);
+        second++;
+    }
+    return len;
+}
