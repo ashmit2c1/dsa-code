7418663d1fe0e19b1876ba01b18f299592b8dc24
Ashmit <codeatashmit@gmail.com>
2024-06-28 09:44:17 +0530

    1-arrays

--- 1-arrays.cpp
+++ 1-arrays.cpp
@@ -267,3 +267,238 @@ int changeAll(vector<int> &v){
 int main(){
     
 }
+// MAX SUBARRAY ( KADANE'S ALGORITHM )
+int maxSubArray(vector<int>& nums) {
+    int maxHere = 0;
+    int maxSoFar = INT_MIN;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        maxHere+=nums[i];
+        if(maxHere<nums[i]){
+            maxHere = nums[i];
+        }
+        if(maxSoFar < maxHere){
+            maxSoFar = maxHere;
+        }
+    }
+    return maxSoFar;
+}
+// REVERSE A GIVEN ARRAY 
+void reverseArray(vector<int>&v){
+    int n = v.size();
+    int start = 0 ;
+    int end = n-1;
+    while(start<=end){
+        swap(v[start],v[end]);
+        start++;
+        end--;
+    }
+    for(int i = 0; i<n ; i++){
+        cout << v[i] << " ";
+    }
+}
+// FIND THE KTH SMALLEST ELEMENT IN ARRAY
+int kthSmallest(vector<int>&v, int k){
+    int n = v.size();
+    sort(v.begin(),v.end());
+    int ans = v[k-1];
+    return ans;
+}
+// FIND THE KTH LARGEST ELEMENT IN ARRAY
+int kthLargest(vector<int>&v, int k){
+    int n = v.size();
+    sort(v.begin(),v.end());
+    int ans = v[n-k+1];
+    return ans;
+}
+// FIND THE UNION AND INTERSECTION OF TWO SORTED ARRAYS AND RETURN THE ARRAY
+vector<int> doUnion(int a[], int n, int b[], int m){
+    int i=0;
+    int j=0;
+    vector<int> unionArr;
+    while(i<n && j<m){
+        if(a[i]<=b[j]){
+            if(unionArr.size()==0 || unionArr.back()!=a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else{
+            if(unionArr.size()==0 || unionArr.back()!=b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+
+        }
+    }
+    while(i<n){
+        if(unionArr.size()==0||unionArr.back()!=a[i]){
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+    while(j<m){
+        if(unionArr.size()==0||unionArr.back()!=b[j]){
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    return unionArr;
+}
+// INTERSECTION OF TWO SORTED ARRAYS
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int n = nums1.size();
+        int m = nums2.size();
+        int i=0;
+        int j=0;
+        vector<int> ans;
+        while(i<n && j<m){
+            if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                if(ans.size()==0|| ans.back()!=nums1[i] ){
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+// CYCLICALLY ROTATE AN ARRAY BY ONE PLACE 
+vector<int> rotate(vector<int>&v){
+    vector<int> ans;
+    int n = v.size();
+    ans.push_back(v[n-1]);
+    for(int i = 0; i<n-1 ; i++){
+        ans.push_back(v[i]);
+    }
+    int index = 0;
+    for(int i = 0; i<n ; i++){
+        v[index]=ans[i];
+    }
+    return v;
+}
+// CHECK IF THE ARRAY IS SORTED OR NOT 
+bool check(vector<int>&v){
+    int n = v.size();
+    for(int i = 0; i<n-1 ; i++){
+        if(v[i] > v[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+// FIND AN ELEMENT IN SORTED ARRAY ( BINARY SEARCH )
+bool find(vector<int>&v,int target){
+    int n = v.size();
+    int start =0;
+    int end = n-1;
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if(v[mid]==target){
+            return true;
+        }
+        if(v[mid]<target){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return false;
+}
+// FIND THE MISSING ELEMENT IN ARAY
+int findMissingNumber(int n,vector<int>&v){
+    int sum = 0;
+    int n2 = v.size();
+    for(int i = 1; i<=n;i++){
+        sum+=i;
+    }
+    int sum2 = 0;
+    for(int i = 0; i<n2 ; i++){
+        sum2+=v[i];
+    }
+    int ans = sum-sum2;
+    return ans;
+}
+// FIND THE MAX ONES BY FLIPPING M ZEROES 
+int findZeros(vector<int> &v, int n, int m){
+    int j=0;
+    int zero=0;
+    int ans=0;
+    for(int i = 0; i<n ; i++){
+        if(v[i]==0){
+            zero++;
+        }
+        while(zero>m){
+            if(v[j]==0){
+                zero--;
+            }
+            j++;
+        }
+        ans=max(ans,j-i+1);
+    }
+    return ans;
+}
+// FIND THE LONGEST SUBARRAY WITH SUM K ( ASSUMING ARRAY HAS ONLY POSITIVES)
+int lenOfLongestSubarray(vector<int>&v, int k){
+    int left = 0;
+    int right = 0;
+    int maxLen = 0;
+    int sum = 0;
+    while(right< v.size()){
+        sum+=v[right];
+        while(left<=right && sum>=k){
+            if(sum==k){
+                maxLen = max(maxLen,right-left+1);
+                right++;
+            }
+            sum-=v[left];
+            left++;
+        }
+        right++;
+    }
+    return maxLen;
+}
+// SEARCH FOR AN ELEMENT IN 2D MATRIX
+bool find(vector<vector<int>> matrix,int target){
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    for(int i=0;i<rows;i++){
+        int start = 0;
+        int end = cols-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(matrix[i][mid]==target){
+                return true;
+            }
+            if(target > matrix[i][mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+    }
+    return false;
+}
+// STOCK BUY AND SELL 
+int stockBuyAndSell(vector<int>&prices){
+    int n= prices.size();
+    int maxProfit = 0;
+    for(int i=1;i<n;i++){
+        if(prices[i]>prices[i-1]){
+            int sell = prices[i]=prices[i-1];
+            maxProfit+=sell;
+        }
+    }
+    return maxProfit;
+}
