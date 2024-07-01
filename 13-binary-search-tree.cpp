2e28adb41e70f03ba1fd4193a16fb9e93ce21db5
Ashmit <codeatashmit@gmail.com>
2024-07-01 15:09:32 +0530

    13-binary-search-tree

--- 13-binary-search-tree.cpp
+++ 13-binary-search-tree.cpp
@@ -81,7 +81,68 @@ int rangeSumBST(TreeNode<int>* root, int left, int right){
     }
     return sum;
 }
-
+// VALIDATE BINARY SEARCH TREE
+bool helper(TreeNode<int>* root, int maxValue = INT_MAX, int minVal = INT_MIN){
+    if(root==NULL){
+        return true;
+    }
+    bool left = helper(root->left,root->data,minVal);
+    bool right = helper(root->right, maxValue,root->data);
+    if(left && right && root->data > minVal && root->data < maxValue){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+// CONVERT A SORTED ARRAY TO BINARY SEARCH TREE
+TreeNode<int>* helperFunction(vector<int>&arr,int start,int end){
+    if(start>end){
+        return NULL;
+    }
+    int mid = start + (end-start)/2;
+    int rootData = arr[mid];
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    root->left = helperFunction(arr,start,mid-1);
+    root->right = helperFunction(arr,mid+1,end);
+    return root;
+}
+TreeNode<int>* createTree(vector<int>&arr){
+    int n = arr.size();
+    return helperFunction(arr,0,n-1);
+}
+// PRINT A BINARY SEARCH TREE
+void printTreeData(TreeNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+    cout << root->data << " ";
+    if(root->left!=NULL){
+        cout << " L " << " : " << root->left->data << "\n";
+    }
+    if(root->right!=NULL){
+        cout << " R " << " : " << root->right->data << "\n";
+    }
+    printTreeData(root->left);
+    printTreeData(root->right);
+}
+// INSERT A DATA IN BINARY SEARCH TREE
+TreeNode<int>* insertData(TreeNode<int>* root, int value){
+    if(root==NULL){
+        TreeNode<int>* n = new TreeNode<int>(value);
+        return n;
+    }
+    if(value > root->data){
+        root->right = insertData(root->right,value);
+    }
+    else{
+        root->left = insertData(root->left,value);
+    }
+    return root;
+}
+bool validate(TreeNode<int>* root){
+    return helper(root);
+}
 int main(){
     
 }
